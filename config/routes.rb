d8f7620dd66203a67b489c6d2b7e5630c0cb81be
Pedro Ulrich <heringer.ulrich@gmail.com>
2024-09-14 19:16:14 -0300

    added initial user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # typed: strict
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resource :users, only: [ :index, :create ]
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
 end
